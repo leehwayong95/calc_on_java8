d927aefe4a79550dc1afd2af75baa14bfef588e3
Lee Hwayong <dlghkdyd500@gmail.com>
2020-11-12 17:30:39 +0900

    add method numberListener, opListener, equalsListener

--- calc/src/calc/CalculatorMain.java
+++ calc/src/calc/CalculatorMain.java
@@ -1,4 +1,5 @@
 package calc;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
@@ -25,8 +26,7 @@ public class CalculatorMain {
 	protected JLabel answer;
 	protected JLabel answerRecord;
 	protected JLabel recordLabel;
-	
-	
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -39,7 +39,7 @@ public class CalculatorMain {
 			}
 		});
 	}
-	
+
 	/**
 	 * @wbp.parser.entryPoint
 	 */
@@ -56,167 +56,216 @@ public class CalculatorMain {
 		frame.getContentPane().setBackground(new Color(188, 188, 188));
 		frame.setBackground(new Color(0, 0, 0));
 		frame.setSize(446, 700);
-		
-		
+
 		JLabel enSharpLabel = new JLabel("EN#");
 		enSharpLabel.setBackground(new Color(0, 191, 255));
 		enSharpLabel.setForeground(new Color(135, 206, 235));
 		enSharpLabel.setBounds(23, 49, 293, 119);
 		frame.getContentPane().add(enSharpLabel);
-		
+
 		answerRecord.setHorizontalAlignment(SwingConstants.RIGHT);
 		answerRecord.setBounds(18, 58, 399, 33);
 		answerRecord.setBorder(new BevelBorder(BevelBorder.LOWERED));
 		frame.getContentPane().add(answerRecord);
-		
-		
 
+		//숫자 1
 		JButton buttonNumber1 = new JButton("1");
 		buttonNumber1.setForeground(new Color(0, 0, 0));
 		buttonNumber1.setBackground(new Color(135, 206, 235));
-		buttonNumber1.addActionListener(new ActionListener() 
-		{
+		buttonNumber1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				((InputedNumberController)calculator).NumberActionListener("1");
+				((InputedNumberController) calculator).NumberActionListener("1");
 			}
 		});
 		buttonNumber1.setBounds(19, 427, 90, 90);
 		frame.getContentPane().add(buttonNumber1);
-		
-		
-		JButton buttonNumber2 = new JButton( "2");
+
+		//숫자 2
+		JButton buttonNumber2 = new JButton("2");
 		buttonNumber2.setBackground(new Color(0, 0, 0));
 		buttonNumber2.setBackground(new Color(135, 206, 235));
-		buttonNumber2.addActionListener(new ActionListener() 
-		{
+		buttonNumber2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				((InputedNumberController)calculator).NumberActionListener("2");
+				((InputedNumberController) calculator).NumberActionListener("2");
 			}
 		});
 		buttonNumber2.setBounds(124, 427, 90, 90);
 		frame.getContentPane().add(buttonNumber2);
 
-		
-		
+		//숫자 3
 		JButton buttonNumber3 = new JButton("3");
 		buttonNumber3.setForeground(new Color(0, 0, 0));
 		buttonNumber3.setBackground(new Color(135, 206, 235));
-		buttonNumber3.addActionListener(new ActionListener() 
-		{
+		buttonNumber3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				((InputedNumberController)calculator).NumberActionListener("3");
+				((InputedNumberController) calculator).NumberActionListener("3");
 			}
 		});
-		buttonNumber3.setBounds(226, 427, 90, 90);
+		buttonNumber3.setBounds(229, 427, 90, 90);
 		frame.getContentPane().add(buttonNumber3);
-		
-		
-		
+
+		//숫자 4
 		JButton buttonNumber4 = new JButton("4");
 		buttonNumber4.setForeground(new Color(0, 0, 0));
 		buttonNumber4.setBackground(new Color(135, 206, 235));
-		buttonNumber4.addActionListener(new ActionListener() 
-		{
+		buttonNumber4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				((InputedNumberController)calculator).NumberActionListener("4");
+				((InputedNumberController) calculator).NumberActionListener("4");
 			}
 		});
-		buttonNumber4.setBounds(19, 315, 90, 90);
+		buttonNumber4.setBounds(19, 322, 90, 90);
 		frame.getContentPane().add(buttonNumber4);
 
-
-		
+		//숫자 5
 		JButton buttonNumber5 = new JButton("5");
 		buttonNumber5.setBackground(new Color(0, 0, 0));
 		buttonNumber5.setBackground(new Color(135, 206, 235));
-		buttonNumber5.addActionListener(new ActionListener() 
-		{
+		buttonNumber5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				((InputedNumberController)calculator).NumberActionListener("5");
+				((InputedNumberController) calculator).NumberActionListener("5");
 			}
 		});
-		buttonNumber5.setBounds(124, 315, 90, 90);
+		buttonNumber5.setBounds(124, 322, 90, 90);
 		frame.getContentPane().add(buttonNumber5);
 
-
+		//숫자 6
 		JButton buttonNumber6 = new JButton("6");
 		buttonNumber6.setForeground(new Color(0, 0, 0));
 		buttonNumber6.setBackground(new Color(135, 206, 235));
-		buttonNumber6.addActionListener(new ActionListener() 
-		{
+		buttonNumber6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				((InputedNumberController)calculator).NumberActionListener("6");
+				((InputedNumberController) calculator).NumberActionListener("6");
 			}
 		});
-		buttonNumber6.setBounds(226, 315, 90, 90);
+		buttonNumber6.setBounds(229, 322, 90, 90);
 		frame.getContentPane().add(buttonNumber6);
-		
-		//¼ýÀÚ7¹öÆ°
+
+		//숫자 7
 		JButton buttonNumber7 = new JButton("7");
 		buttonNumber7.setForeground(new Color(0, 0, 0));
 		buttonNumber7.setBackground(new Color(135, 206, 235));
-		buttonNumber7.setBounds(19, 204, 90, 90);
-		buttonNumber7.addActionListener(new ActionListener() 
-		{
+		buttonNumber7.setBounds(19, 217, 90, 90);
+		buttonNumber7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				((InputedNumberController)calculator).NumberActionListener("7");
+				((InputedNumberController) calculator).NumberActionListener("7");
 			}
 		});
 		frame.getContentPane().setLayout(null);
 		frame.getContentPane().add(buttonNumber7);
-		
-		//¼ýÀÚ8¹öÆ°
+
+		//숫자 8
 		JButton buttonNumber8 = new JButton("8");
 		buttonNumber8.setForeground(new Color(0, 0, 0));
 		buttonNumber8.setBackground(new Color(135, 206, 235));
-		buttonNumber8.addActionListener(new ActionListener() 
-		{
+		buttonNumber8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				((InputedNumberController)calculator).NumberActionListener("8");
+				((InputedNumberController) calculator).NumberActionListener("8");
 			}
 		});
-		buttonNumber8.setBounds(124, 204, 90, 90);
+		buttonNumber8.setBounds(124, 217, 90, 90);
 		frame.getContentPane().add(buttonNumber8);
-		
-		//¼ýÀÚ9¹öÆ°
+
+		// 숫자9
 		JButton buttonNumber9 = new JButton("9");
 		buttonNumber9.setForeground(new Color(0, 0, 0));
 		buttonNumber9.setBackground(new Color(135, 206, 235));
-		buttonNumber9.addActionListener(new ActionListener() 
-		{
+		buttonNumber9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				((InputedNumberController)calculator).NumberActionListener("9");
+				((InputedNumberController) calculator).NumberActionListener("9");
 			}
 		});
-		buttonNumber9.setBounds(226, 204, 90, 90);
+		buttonNumber9.setBounds(229, 217, 90, 90);
 		frame.getContentPane().add(buttonNumber9);
 
+		//숫자 0
+		JButton buttonNumber0 = new JButton("0");
+		buttonNumber0.setForeground(new Color(0, 0, 0));
+		buttonNumber0.setBackground(new Color(135, 206, 235));
+		buttonNumber0.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				((InputedNumberController) calculator).NumberActionListener("0");
+			}
+		});
+		buttonNumber0.setBounds(124, 532, 90, 90);
+		frame.getContentPane().add(buttonNumber0);
 		
+		//덧셈
+		JButton buttonNumberSum = new JButton("+");
+		buttonNumberSum.setForeground(new Color(0, 0, 0));
+		buttonNumberSum.setBackground(new Color(135, 206, 235));
+		buttonNumberSum.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				((InputedNumberController) calculator).OpActionListener("+");
+			}
+		});
+		buttonNumberSum.setBounds(334, 217, 90, 90);
+		frame.getContentPane().add(buttonNumberSum);
 		
+		//뺄셈
+		JButton buttonNumberSub = new JButton("-");
+		buttonNumberSub.setForeground(new Color(0, 0, 0));
+		buttonNumberSub.setBackground(new Color(135, 206, 235));
+		buttonNumberSub.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				((InputedNumberController) calculator).OpActionListener("-");
+			}
+		});
+		buttonNumberSub.setBounds(334, 322, 90, 90);
+		frame.getContentPane().add(buttonNumberSub);
 		
+		//곱셈
+		JButton buttonNumberMul = new JButton("x");
+		buttonNumberMul.setForeground(new Color(0, 0, 0));
+		buttonNumberMul.setBackground(new Color(135, 206, 235));
+		buttonNumberMul.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				((InputedNumberController) calculator).OpActionListener("*");
+			}
+		});
+		buttonNumberMul.setBounds(334, 427, 90, 90);
+		frame.getContentPane().add(buttonNumberMul);
 		
+		//나눗셈
+		JButton buttonNumberDiv = new JButton("÷");
+		buttonNumberDiv.setForeground(new Color(0, 0, 0));
+		buttonNumberDiv.setBackground(new Color(135, 206, 235));
+		buttonNumberDiv.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				((InputedNumberController) calculator).OpActionListener("/");
+			}
+		});
+		buttonNumberDiv.setBounds(334, 532, 90, 90);
+		frame.getContentPane().add(buttonNumberDiv);
 		
-		frame.addKeyListener(new KeyAdapter()
-		{
+		//=
+		JButton buttonNumberEqu = new JButton("=");
+		buttonNumberEqu.setForeground(new Color(0, 0, 0));
+		buttonNumberEqu.setBackground(new Color(135, 206, 235));
+		buttonNumberEqu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				((InputedNumberController) calculator).EquActionListener();
+			}
+		});
+		buttonNumberEqu.setBounds(229, 112, 195, 90);
+		frame.getContentPane().add(buttonNumberEqu);
+		
+		frame.addKeyListener(new KeyAdapter() {
 			@Override
-			public void keyPressed(KeyEvent event)
-			{
+			public void keyPressed(KeyEvent event) {
 				char inputedKey = event.getKeyChar();
 				int key = event.getKeyCode();
 
-				if(inputedKey>='0' && inputedKey <='9') {
+				if (inputedKey >= '0' && inputedKey <= '9') {
 					String keyString = "";
-					keyString+=inputedKey;
-					((InputedNumberController)calculator).NumberActionListener(keyString);
+					keyString += inputedKey;
+					((InputedNumberController) calculator).NumberActionListener(keyString);
 				}
 
+				else if (key == KeyEvent.VK_ENTER) {
 
-				else if(key == KeyEvent.VK_ENTER)
-				{
-					
 				}
 			}
-				
+
 		});
 	}
 }
--- calc/src/calc/InputedNumberController.java
+++ calc/src/calc/InputedNumberController.java
@@ -4,18 +4,159 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 public class InputedNumberController extends CalculatorMain {
-
+	String tmpInteger = "";
+	ArrayList<Integer> numbers = new ArrayList<Integer>();
+	ArrayList<String> op = new ArrayList<String>();
+	boolean repeatFlag = false; // equals 반복 연산시
+	boolean opeditFlag = false; // 연산자 수정시
+	int repeatresult = 0; //반복연산을 위한 결과값 저장소
 
 	public void NumberActionListener(String number)
 	{
+		if(repeatFlag)
+			reset_List();
 		frame.requestFocus();
 		frame.setFocusable(true);
-		
-		answerRecord.setText( number);
-		
+		this.tmpinputNumber(number);
+		answerRecord.setText(tmpInteger); //화면 숫자 반환
 	}
 	
-
+	public void OpActionListener(String op)
+	{
+		repeatFlag = false;
+		repeatresult = 0;
+		frame.requestFocus();
+		frame.setFocusable(true);
+		if(!opeditFlag)
+			this.inputOp(op);
+		else
+			this.editOp(op);
+		answerRecord.setText(op);
+	}
+	
+	public void EquActionListener()
+	{
+		frame.requestFocus();
+		frame.setFocusable(true);
+		if(!repeatFlag)
+		{
+			String result = calresult();
+			answerRecord.setText(result);
+			//System.out.println("RESULT : "+result);//DEBUG
+		}
+		else
+		{
+			String result = repeatCal();
+			answerRecord.setText(result);
+		}
+	}
+	
+	//숫자 입력 미완
+	void tmpinputNumber(String input)
+	{
+		opeditFlag = false;
+		tmpInteger += input;
+	}
+	
+	//연산자 입력시 실행
+	void inputNumber()
+	{
+		if(tmpInteger.equals(""))
+			numbers.add(0);
+		else
+		{
+			try {
+				numbers.add(Integer.parseInt(tmpInteger));
+			}
+			catch(Exception e)
+			{
+				System.out.println("fuck");
+			}
+		}
+		tmpInteger="";
+	}
+	
+	//연산 대기 큐
+	void inputOp(String input)
+	{
+		this.inputNumber();//입력중인 숫자 list에 넣기
+		op.add(input);
+		opeditFlag = true;
+	}
+	
+	//연산자 수정 큐
+	void editOp(String input)
+	{
+		op.set(op.size()-1, input);
+		opeditFlag = true;
+	}
+	
+	//equals 입력시 연산 시작
+	String calresult ()
+	{
+		this.inputNumber(); //입력중인 숫자 list에 넣기
+		int result = 0;
+		if(numbers.size() == 1||op.size() == 0) {
+			return Integer.toString(numbers.get(0));
+		}
+		else
+		{
+			int size = numbers.size();
+			for(int i = 0 ; i < size; i++)
+			{
+				if(i == 0)
+					result += numbers.get(0);
+				else
+				{
+					if(op.get(i-1).equals("+"))
+						result += numbers.get(i);
+					else if(op.get(i-1).equals("-"))
+						result -= numbers.get(i);
+					else if(op.get(i-1).equals("*"))
+						result *= numbers.get(i);
+					else if(op.get(i-1).equals("/"))
+					{
+						try {
+							result /= numbers.get(i);
+						}
+						catch (Exception e)
+						{
+							System.out.println("0으로 나누기 시도");
+						}
+					}
+				}
+			}
+			repeatFlag = true;
+			repeatresult = result; //반복 연산시..
+			return Integer.toString(result);
+		}
+	}
+	
+	String repeatCal()
+	{
+		int x = numbers.get(numbers.size()-1);
+		String operation = op.get(op.size()-1);
+		if(operation.equals("+"))
+			repeatresult += x;
+		else if(operation.equals("-"))
+			repeatresult -= x;
+		else if(operation.equals("*"))
+			repeatresult *= x;
+		else if(operation.equals("/"))
+			repeatresult /= x;
+		
+		return Integer.toString(repeatresult);
+	}
+	
+	void reset_List()
+	{
+		numbers.clear();
+		op.clear();
+		repeatresult = 0;
+		repeatFlag = false;
+	}
+	
 }
